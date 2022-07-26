d4ed4d772c7dc5538c51809a94006fed62e1d046
IlyaKiz <iliakiz@mail.ru>
2022-07-26 10:30:03 +0300

    31_clear_test_database_using_rollback

--- src/test/java/ru/mis2022/util/ContextIT.java
+++ src/test/java/ru/mis2022/util/ContextIT.java
@@ -1,16 +1,20 @@
 package ru.mis2022.util;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.flyway.FlywayMigrationStrategy;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Import;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.transaction.annotation.Transactional;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 
 @SpringBootTest
 @AutoConfigureMockMvc
 @ActiveProfiles("test")
+@Transactional
 public class ContextIT {
 
     @Autowired protected ITAccessTokenUtil tokenUtil;
